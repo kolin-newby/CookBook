2f936d03a6f14cea55faa8e95644390098b53fcb
Kolin Newby <kolin.newby@gmail.com>
2026-01-07 10:13:26 -0700

    Rework about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -19,14 +19,23 @@ const About = () => {
           />
         </div>
       </div> */}
-      <article className="max-h-full overflow-y-scroll flex-col px-2 py-4 text-theme-5 sm:max-w-2/3 w-full flex-1 sm:space-y-4 text-sm sm:text-base md:text-lg text-wrap">
-        <div className="relative float-left p-3 mr-3 mb-1 w-1/2 aspect-square">
+      <div className="flex flex-row sm:flex-col w-11/12 sm:w-1/2 md:w-1/2 items-center justify-center">
+        <div className="relative flex p-2 w-1/2 max-w-[220px] sm:w-full aspect-square">
           <div className="absolute top-0 left-0 w-full h-full rounded-[40%] border-20 sm:border-40 border-theme-3 animate-spin-xslow" />
           <img
             src="/photos/JZ.jpg"
             className=" object-cover size-full rounded-[40%]"
           />
         </div>
+        <div className="relative flex p-2 w-1/2 max-w-[220px] sm:w-full aspect-square">
+          <div className="absolute top-0 left-0 w-full h-full rounded-[40%] border-20 sm:border-40 border-theme-4 animate-spin-xslow-reverse" />
+          <img
+            src="/photos/KF.jpg"
+            className=" object-cover size-full rounded-[40%]"
+          />
+        </div>
+      </div>
+      <article className="max-h-full space-y-3 rounded-[50px] bg-linear-to-br from-theme-2/70 to-theme-2/20 py-4 px-6 overflow-y-scroll flex-col inset-shadow-sm text-theme-5 sm:max-w-2/3 w-full sm:w-full sm:space-y-4 text-sm sm:text-base md:text-lg">
         <p>
           This is a short exerpt on what this website is and who it is
           for...maybe say hi to some of our friends or maybe even explain what
@@ -39,13 +48,6 @@ const About = () => {
           for...maybe say hi to some of our friends or maybe even explain what
           kind of recipes are on here.
         </p>
-        <div className="relative float-right p-3 ml-3 my-1 w-1/2 aspect-square">
-          <div className="absolute top-0 left-0 w-full h-full rounded-[40%] border-20 sm:border-40 border-theme-4 animate-spin-xslow-reverse" />
-          <img
-            src="/photos/KF.jpg"
-            className=" object-cover size-full rounded-[40%]"
-          />
-        </div>
         <p>
           This is a short exerpt on what this website is and who it is
           for...maybe say hi to some of our friends or maybe even explain what
