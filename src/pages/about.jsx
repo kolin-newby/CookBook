24d053e40bfef3cc808d425d072798fa719abc18
Kolin Newby <kolin.newby@gmail.com>
2026-01-06 18:18:04 -0700

    again

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -20,7 +20,7 @@ const About = () => {
         </div>
       </div> */}
       <article className="max-h-full overflow-y-scroll flex-col px-1 py-4 text-theme-5 sm:max-w-2/3 w-full flex-1 sm:space-y-4 text-sm sm:text-base md:text-lg text-wrap">
-        <div className="relative float-left p-3 mr-3 w-1/2 aspect-square">
+        <div className="relative float-left p-3 mr-3 mb-1 w-1/2 aspect-square">
           <div className="absolute top-0 left-0 w-full h-full rounded-[40%] border-20 sm:border-40 border-theme-3 animate-spin-xslow" />
           <img
             src="/photos/JZ.jpg"
@@ -39,7 +39,7 @@ const About = () => {
           for...maybe say hi to some of our friends or maybe even explain what
           kind of recipes are on here.
         </p>
-        <div className="relative float-right p-3 mr-3 w-1/2 aspect-square">
+        <div className="relative float-right p-3 ml-3 my-1 w-1/2 aspect-square">
           <div className="absolute top-0 left-0 w-full h-full rounded-[40%] border-20 sm:border-40 border-theme-4 animate-spin-xslow-reverse" />
           <img
             src="/photos/KF.jpg"
